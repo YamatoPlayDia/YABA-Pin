c0b6b3dd65d4ed497c9640dd088bfaaa03a14e2c
konishimari <mari.konishi.works@gmail.com>
2023-07-01 22:53:37 +0900

    失敗分を消しましょう。

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/lunchbox', function () {
+    return view('lunchbox');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/create-spot', function () {
     return view('create_spot');
