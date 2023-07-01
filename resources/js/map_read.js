f64e3ddcc2b57c149c9c0373f63eb7611458a7e5
YamatoPlayDia <wekoto.mu@gmail.com>
2023-07-01 09:54:46 +0900

    方位磁針・カードなど

--- resources/js/map_read.js
+++ resources/js/map_read.js
@@ -49,13 +49,13 @@ function animate() {
 }
 animate();
 
-
 async function initMap() {
   // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
     const { PlacesService, PlacesServiceStatus } = await google.maps.importLibrary("places");
-
+    let promises = [];
+    let cardsData = [];
     // Use the browser's built-in Geolocation API to get the current location.
     navigator.geolocation.getCurrentPosition(async function (position) {
         const currentPosition = {
@@ -107,6 +107,7 @@ async function initMap() {
             console.log(currentPosition);
         });
         // Create a circle centered at the current position.
+        const circleRadius = 200;
         const circle = new google.maps.Circle({
             strokeColor: '#db8555',
             strokeOpacity: 0.8,
@@ -115,9 +116,8 @@ async function initMap() {
             fillOpacity: 0.15,
             map: map,
             center: currentPosition,
-            radius: 200
+            radius: circleRadius,
         });
-
         google.maps.event.addListener(circle, 'click', function() {
             markers.forEach(marker => {
                 const isInCircle = google.maps.geometry.spherical.computeDistanceBetween(marker.position, circle.getCenter()) <= circle.getRadius();
@@ -126,6 +126,45 @@ async function initMap() {
                 }
             });
         });
+        // Directions and their corresponding degrees
+        const directions = {
+            '北': 90,
+            '東': 180,
+            '南': 270,
+            '西': 0,
+        };
+
+        // Function to calculate point on circle for given center and radius
+        function calculatePoint(center, angleInDegrees, radius) {
+            const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+            const latRadius = radius / 111325; // divide by ~111325 to convert radius from meters to degrees
+            const lngRadius = latRadius / Math.cos(center.lat * Math.PI / 180); // adjust for Earth's sphericity
+            return {
+            lat: center.lat + (latRadius * Math.cos(angleInRadians)),
+            lng: center.lng + (lngRadius * Math.sin(angleInRadians)),
+            };
+        }
+
+        // Loop over each direction and create a marker
+        for (const [direction, degree] of Object.entries(directions)) {
+            // Calculate point on circle for current direction
+            const point = calculatePoint(currentPosition, degree, circleRadius);
+
+            // Create SVG for the marker
+            const directionSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><text x="50%" y="50%" text-anchor="middle" dominant-baseline="central" font-size="24" font-weight="bold" stroke="#f8c967" stroke-width="1" fill="#db8555" fill-opacity="0.8">${direction}</text></svg>`;
+
+            // Create the marker for the direction
+            const directionMarker = new google.maps.Marker({
+            position: point,
+            map,
+            icon: {
+                url: 'data:image/svg+xml,' + encodeURIComponent(directionSvg),
+                scaledSize: new google.maps.Size(22, 22),
+            },
+            });
+        }
+
+
 
         // Use the PlacesService to find nearby parks.
         const { RankBy } = await google.maps.importLibrary("places");
@@ -140,7 +179,10 @@ async function initMap() {
         let placeIds = []; // To store unique placeIds
         let nextPageToken = null;
         let delayInMilliseconds = 2000; //two seconds
-        placesService.nearbySearch(request, (results, status, pagination) => {
+        // An object to store markers by placeId for easy access
+        let markersByPlaceId = {};
+
+        placesService.nearbySearch(request, (results, status) => {
             if (status === PlacesServiceStatus.OK) {
                 for (let i = 0; i < results.length; i++) {
                     if (!results[i].types.some(type => request.type.includes(type))) {
@@ -159,6 +201,7 @@ async function initMap() {
                         scale: 1.0,
                     });
 
+                    // Marker creation
                     const marker = new AdvancedMarkerElement({
                         map,
                         content: placePin.element,
@@ -167,6 +210,17 @@ async function initMap() {
 
                     marker.placeId = results[i].place_id;
                     markers.push(marker);
+                    markersByPlaceId[marker.placeId] = marker;  // Store the marker by its placeId
+
+                    // Add a click listener to the marker to scroll to its corresponding card
+                    marker.addListener('click', () => {
+                        const card = document.querySelector(`.card[data-place-id="${marker.placeId}"]`);
+                        const scrollContainer = document.querySelector('#cards-slider');
+
+                        // Calculate where to scroll. This centers the card in the container.
+                        const scrollPosition = card.offsetLeft - (scrollContainer.offsetWidth / 2) + (card.offsetWidth / 2);
+                        scrollContainer.scrollTo({ left: scrollPosition, behavior: 'smooth' });
+                    });
 
                     function getDirection(degrees) {
                         const directions = ['北', '北東', '東', '南東', '南', '南西', '西', '北西'];
@@ -174,40 +228,95 @@ async function initMap() {
                         return directions[index];
                     }
 
-                    placesService.getDetails({ placeId: marker.placeId }, (place, status) => {
-                        if (status === PlacesServiceStatus.OK) {
-                            // Remove postal code and country from the address.
-                            const addressParts = place.formatted_address.split(',');
-                            const address = addressParts.slice(0, -2).join(',').trim();
+                    // Create an array to store the data.
+                    let promise = new Promise((resolve, reject) => {
+                        placesService.getDetails({ placeId: marker.placeId }, (place, status) => {
+                            if (status === PlacesServiceStatus.OK) {
+                                // Remove postal code and country from the address.
+                                const addressParts = place.formatted_address.split(',');
+                                const address = addressParts.slice(0, -2).join(',').trim();
 
-                            // Calculate distance in meters and round down to the nearest whole number.
-                            const distance = Math.floor(google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(currentPosition), place.geometry.location));
+                                // Calculate distance in meters and round down to the nearest whole number.
+                                const distance = Math.floor(google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(currentPosition), place.geometry.location));
 
-                            // Calculate direction and convert to 8-direction format.
-                            const directionDegrees = google.maps.geometry.spherical.computeHeading(new google.maps.LatLng(currentPosition), place.geometry.location);
-                            const direction = getDirection(directionDegrees);
+                                // Calculate direction and convert to 8-direction format.
+                                const directionDegrees = google.maps.geometry.spherical.computeHeading(new google.maps.LatLng(currentPosition), place.geometry.location);
+                                const direction = getDirection(directionDegrees);
 
-                            console.log('PlaceId:', place.place_id);
-                            console.log('Name:', place.name);
-                            console.log('Address:', address);
-                            console.log('Latitude:', place.geometry.location.lat());
-                            console.log('Longitude:', place.geometry.location.lng());
-                            console.log('Distance:', distance + 'm');
-                            console.log('Direction:', direction);
-                            console.log('Photo URL:', place.photos ? place.photos[0].getUrl() : 'No Photo Available');
-                        }
+                                cardsData.push({
+                                    placeId: place.place_id,
+                                    name: place.name,
+                                    address: address,
+                                    latitude: place.geometry.location.lat(),
+                                    longitude: place.geometry.location.lng(),
+                                    distance: distance + 'm',
+                                    direction: direction,
+                                    photoURL: place.photos ? place.photos[0].getUrl() : '/img/type-park.png',
+                                    isInCircle: isInCircle,
+                                });
+                                resolve(); // Resolve the promise when the detail request is done.
+                            } else {
+                                reject(); // Reject the promise if there is an error.
+                            }
+                        });
                     });
-                }
 
-                if (pagination.hasNextPage) {
-                    nextPageToken = pagination.nextPageToken;
-                    setTimeout(() => {
-                        pagination.nextPage();
-                    }, delayInMilliseconds);
+                    // Add the promise to the promises array.
+                    promises.push(promise);
                 }
+                // When all the promises are done, sort the data and generate the cards.
+                Promise.all(promises).then(() => {
+
+                    // Sort the data by distance.
+                    cardsData.sort((a, b) => parseInt(a.distance) - parseInt(b.distance));
+
+                    // Clear the current cards.
+                    document.querySelector('#cards-slider').innerHTML = '';
+
+                    var style = document.createElement('style');
+                    style.id = 'cards-style';
+                    style.innerHTML = `
+                    #cards-slider > div {
+                        visibility: hidden;
+                    }
+                    `;
+                    document.head.appendChild(style);
+
+                    // Generate a new card for each data point and add it to the #cards-slider element.
+                    for (let cardData of cardsData) {
+                        const card = `<div class="card flex-none w-64 h-48 mr-4 mb-4 bg-white rounded-xl shadow-lg flex flex-col justify-between" data-place-id="${cardData.placeId}">
+                            <img src="${cardData.photoURL}" class="w-full h-2/5 object-cover rounded-t-xl">
+                            <div class="p-3">
+                                <p class="text-xs text-gray-500">${cardData.name} - ${cardData.direction} ${cardData.distance}</p>
+                                <h2 class="text-lg font-semibold">みかいたくのち</h2>
+                                <p class="text-xs text-gray-400">${cardData.address}</p>
+                            </div>
+                            <button class="p-3 text-sm ${cardData.isInCircle ? 'bg-blue-500' : 'bg-gray-500'} text-white rounded-b-xl" ${cardData.isInCircle ? '' : 'disabled'}>拾う</button>
+                        </div>`;
+                        document.querySelector('#cards-slider').innerHTML += card;
+                    }
+
+                    window.getComputedStyle(document.querySelector('#cards-slider > div')).visibility;
+
+                    // 非表示のスタイルを削除し、HTML要素を表示します。
+                    document.getElementById('cards-style').remove();
+
+                    // Add mouseover and mouseout listeners to each card to highlight and unhighlight its corresponding marker
+                    const cards = document.querySelectorAll('.card');
+                    cards.forEach(card => {
+                        card.addEventListener('click', () => {
+                            const marker = markersByPlaceId[card.dataset.placeId];
+                            marker.content.style.transform = 'scale(1.5)';  // Increase size of icon
+                        });
+                        google.maps.event.addListener(map, 'click', () => {
+                            for (let placeId in markersByPlaceId) {
+                                markersByPlaceId[placeId].content.style.transform = 'scale(1.0)';  // Reset size of icon
+                            }
+                        });
+                    });
+                });
             }
         });
-
     });
 };
   // Create zoom in and zoom out functionalities
@@ -215,7 +324,7 @@ async function initMap() {
 document.getElementById('rotate-left').addEventListener('click', function() {
     map.setHeading(map.getHeading() - 45);
     model.rotation.y -= Math.PI / 4;
-});
+});https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sAaw_FcLkPCt-_A0rn-tb7mKPiAy4ueVjD9nwPNYwDALR98Z8Bka-OsMi4r1tJ6zXUTW_Kd_jJVP_TnGcUY3OQZ5thfWxUC2A7WWcE5nxKi4uZCPAYx0T9SAKtT61yHTC6HK8VbpE10Rgxhh2Z-pKoIiel3tpovFReYhnts0bZ29enPsmgDcB&3u3072&5m1&2e1&callback=none&key=AIzaSyCAG2YEDYixE9zNbBckshdHLm3AHlLHuyE&token=74411
 
 document.getElementById('rotate-right').addEventListener('click', function() {
     map.setHeading(map.getHeading() + 45);
