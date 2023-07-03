bd2766824ddcaba49a58ddcdfc938a05567da4ae
Kawarai-S <kayawakakururu@icloud.com>
2023-07-03 22:33:36 +0900

    ボタンを追加

--- resources/js/typing.js
+++ resources/js/typing.js
@@ -6,6 +6,8 @@ window.onload = () => {
     const pTags = Array.from(messageContainer.getElementsByTagName('p'));
     let currentTagIndex = 0;
 
+    const revealButton = document.querySelector('.yabapin_btn'); // ボタンを取得
+
     const typeMessage = (pTag, index = 0) => {
         const text = pTag.dataset.text;
         if (index < text.length) {
@@ -14,6 +16,13 @@ window.onload = () => {
         } else if (currentTagIndex < pTags.length - 1) {
             currentTagIndex += 1;
             setTimeout(() => typeMessage(pTags[currentTagIndex]), 1000);
+        } else {
+            if(revealButton !== null) {  // ボタンがページ上に存在する場合のみ
+                revealButton.style.display = "inline-block";
+                setTimeout(() => {
+                    revealButton.style.opacity = "1";
+                }, 500);
+            }
         }
     };
 
